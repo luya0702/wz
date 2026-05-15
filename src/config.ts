8422ace92220da003e379e690c3d3d54228adf45
luya0702 <2976977535@qq.com>
2026-05-15 15:37:51 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -59,14 +59,14 @@ export const profileConfig: ProfileConfig = {
 	links: [
 		{
 			name: "bilibili",
-			icon: "qlementine-icons:format-bold-16", // Visit https://icones.js.org/ for icon codes
+			icon: "tabler:brand-bilibili", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
 			url: "https://space.bilibili.com/336030665",
 		},
 		{
 			name: "Telegram",
-			icon: "logos:telegram",
+			icon: "ic:baseline-telegram",
 			url: "https://t.me/sharegal",
 		},
 	],
