9e4c256eccf57e4056410e73899652994276eb8c
luya0702 <2976977535@qq.com>
2026-05-15 14:55:49 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/tx.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "亞",
 	bio: "专注于分享各类galgame",
 	links: [
